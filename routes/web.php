00144a1b19c8269ae5668c80e333cd543a4ada56
Krzysztof <krzysztof@107.pl>
2024-10-10 20:19:08 +0200

    Pull and install of breeze with php artisan breeze:install

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\CoordinatorController;
-use App\Http\Controllers\AdministratorController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get("/", [UserController::class, 'index'])->name('user.user');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-// todo: dodać pewien sposób uwierzytelnienia do koordynatorów
-// Route::get("/coordinator", [CoordinatorController::class, 'index'])->middleware(['auth', 'verified'])->name('coordinator.coordinator');
-Route::get("/coordinator", [CoordinatorController::class, 'index'])->name('coordinator.coordinator');
-
-
-// todo: dodać pewien sposób uwierzytelnienia do koordynatorów
-// Route::get("/administrator", [AdministratorController::class, 'index'])->middleware(['auth', 'verified'])->name('administrator.administrator');
-Route::get("/administrator", [AdministratorController::class, 'index'])->name('administrator.administrator');
+require __DIR__.'/auth.php';

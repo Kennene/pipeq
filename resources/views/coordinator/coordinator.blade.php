249e2f722eb0f9effe540a490ea30088d37ca2ce
Krzysztof <krzysztof@107.pl>
2024-10-29 19:56:48 +0100

    Add PipeQ unfinished facade to coordinator blade

--- resources/views/coordinator/coordinator.blade.php
+++ resources/views/coordinator/coordinator.blade.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script> <!-- Potrzebne do wysyłania zapytań POST -->
     <title>Coordinator</title>
 </head>
 
@@ -19,5 +20,34 @@
     </div>
 
 </body>
+
+
+<script>
+
+    class PipeQ {
+        constructor() {
+            const channel = Echo.private('register');
+            this.register = channel;
+        }
+
+        _move($ticket_id, $destination) {
+            axios.post(`/move/$`)
+                .then(response => {
+                    @if(env('APP_DEBUG'))
+                        console.log(response);
+                    @endif
+                })
+                .catch(error => {
+                    console.error(error.response.data.error);
+                });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    PipeQ = new PipeQ();
+});
+
+
+</script>
 
 </html>

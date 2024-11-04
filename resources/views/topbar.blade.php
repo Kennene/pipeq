9624765b6c4fb7432c717067d022a2953d7029d9
Krzysztof Pacyna <krzysztof@107.pl>
2024-11-04 22:30:57 +0100

    Better topbar with language switcher, which doesn't work... yet

--- resources/views/topbar.blade.php
+++ resources/views/topbar.blade.php
@@ -1,19 +1,26 @@
-<style>
-    :root {
-        --main: {{ \App\Models\Color::main }};
-        --secondary: {{ \App\Models\Color::secondary }};
-        --additional: {{ \App\Models\Color::additional }};
-        --details: {{ \App\Models\Color::details }};
-        --white: {{ \App\Models\Color::white }};
-        --black: {{ \App\Models\Color::black }};
-    }
-</style>
-
 @vite(['resources/css/topbar.css'])
 
-<nav id="topbar" class="p-4 flex justify-between items-center shadow-lg relative">
-    <div class="flex items-center">
-        <img src="/images/logo.png" alt="Logo" class="h-12" />
-        <h1 class="ml-4 text-xl font-bold">PipeQ</h1>
+<nav id="topbar" class="navbar py-2 bg-dark">
+    <div class="container-fluid">
+        <div class="navbar-brand d-flex align-items-center">
+            <img id="app-logo" src="/images/logo.png" class="img-fluid" style="height: 40px;" />
+            <div class="app-name ms-2">
+                <h1 class="h4 mb-0 text-light">{{ env('APP_NAME') }}</h1>
+            </div>
+        </div>
+
+        <div class="dropdown">
+            <a class="btn btn-primary dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Language</a>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                @foreach (config('app.available_locales') as $locale => $language)
+                    @if ($locale === App::getLocale())
+                        <a class="dropdown-item active" href="">{{ $language }}</a>
+                    @else
+                        <a class="dropdown-item" href="">{{ $language }}</a>
+                    @endif
+                @endforeach
+            </div>
+        </div>
+        
     </div>
 </nav>

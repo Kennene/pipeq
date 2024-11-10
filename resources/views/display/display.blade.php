1ac7ff1b34712e70122a5557078900c6ad7d6c50
Krzysztof Pacyna <krzysztof@107.pl>
2024-11-10 16:18:52 +0100

    Code cleanup of PipeQ facade for better visbility and performance on display blade

--- resources/views/display/display.blade.php
+++ resources/views/display/display.blade.php
@@ -24,70 +24,53 @@
 
 var tickets = @json($tickets).map(ticket => ticket);
 
-
-
-tickets.forEach(ticket => {
-    let ticket_card = document.getElementById(`ticket${ticket.id}`);
-
-    if(!ticket_card) {
-        ticket_card = appendTicketTemplate(ticket.id);
-    }
-
-    if(!ticket.workstation) {
-        ticket.workstation = ticket.destination;
-    }
-    
-    ticket_card.querySelector('h5').textContent = ticket.user;
-    ticket_card.querySelector('p').textContent = ticket.status;
-    ticket_card.querySelector('h6').textContent = ticket.workstation;
-
-});
-
-function appendTicketTemplate(ticket_id) {
-    $('#tickets-holder').append(`
-        <div id="ticket${ticket_id}" value="${ticket_id}" class="ticket-card card m-3 shadow-lg">
-            <div class="card-body p-3">
-                <h5 class="h2 card-title text-center"></h5>
-                <hr>
-                <p class="card-text mt-2"></p>
-                <h6 class="card-subtitle mt-1"></h6>
-            </div>
-        </div>
-    `);
-
-    return document.getElementById(`ticket${ticket_id}`);
-}
-
 class PipeQ {
     constructor() {
+
         const channel = Echo.private(`display`);
         this.display = channel;
-        this._updateDisplay();
+
+        // for each ticket, create a card
+        tickets.forEach(ticket => {
+            this._updateTicket(ticket);
+        });
+
+        // listen for changes of tickets
+        this._listenForChanges();
     }
 
-    _updateDisplay() {
+    _listenForChanges() {
         this.display.listen('UpdateDisplayAboutTicket', function(e) {
-            @if(env('APP_ENV'))
-                console.log(e);
-            @endif
-
-            let ticket = e.ticket;
-            let ticket_card = document.getElementById(`ticket${ticket.id}`);
-
-            // if ticket does nit exist, create it
-            if(!ticket_card) {
-                ticket_card = appendTicketTemplate(ticket.id);
-            }
-
-            // if workstation is not set, set it to destination
-            if(!ticket.workstation) {
-                ticket.workstation = ticket.destination;
-            }
-
-            ticket_card.querySelector('h5').textContent = ticket.user;
-            ticket_card.querySelector('p').textContent = ticket.status;
-            ticket_card.querySelector('h6').textContent = ticket.workstation;
+            console.log('New update:', e);
+            this._updateTicket(e.ticket);
         })
+    }
+
+    _updateTicket(ticket) {
+        let ticket_card = document.getElementById(`ticket${ticket.id}`);
+
+        if(!ticket_card) {
+            $('#tickets-holder').append(`
+                <div id="ticket${ticket.id}" value="${ticket.id}" class="ticket-card card m-3 shadow-lg">
+                    <div class="card-body p-3">
+                        <h5 class="h2 card-title text-center"></h5>
+                        <hr>
+                        <p class="card-text mt-2"></p>
+                        <h6 class="card-subtitle mt-1"></h6>
+                    </div>
+                </div>
+            `);
+
+            ticket_card = document.getElementById(`ticket${ticket.id}`);
+        }
+
+        if(!ticket.workstation) {
+            ticket.workstation = ticket.destination;
+        }
+
+        ticket_card.querySelector('h5').textContent = ticket.user;
+        ticket_card.querySelector('p').textContent = ticket.status;
+        ticket_card.querySelector('h6').textContent = ticket.workstation;
     }
 }
 

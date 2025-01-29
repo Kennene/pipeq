78fb57fe68bb53e71b166cd7182d5b8e98d350d5
Krzysztof <krzysztof@107.pl>
2025-01-29 21:47:06 +0100

    Change the way CSS is generated from dynamic colors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 
+use App\Models\Color;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,12 +23,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Blade::directive('pipeQColors', function () {
-            $colors = config('colors');
-            $pipeQColors = collect($colors)->map(function ($value, $key) {
-                return "--{$key}: {$value};";
+            $colors = Color::all();
+
+            $pipeQColors = $colors->map(function ($colors) {
+                return "--{$colors->name}: {$colors->hex_code};";
             })->implode(' ');
-    
-            return "<style>:root { {$pipeQColors} }</style>";
+
+            return "<style>:root { $pipeQColors }</style>";
         });
     }
 }
